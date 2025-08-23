496130e6fb02a555e8619810b2f64c6d4212a652
SerhiiKutovyi <wagenrex@gmail.com>
2025-08-23 17:41:48 +0300

    hw-7/5

---
+++ js/task-5.js
@@ -0,0 +1,18 @@
+const bodyRef = document.querySelector('body');
+
+const buttonChangeColorRef = document.querySelector('.change-color');
+
+const colorRef = document.querySelector('.color');
+
+// bodyRef.style.backgroundColor = 'red';
+
+buttonChangeColorRef.addEventListener('click', e => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorRef.innerHTML = bodyRef.style.backgroundColor;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
